d3e8cc3915449b58ab15d5bc9c33ea97781ba029
sourjo-ghosh <joyghosh211@outlook.com>
2026-02-22 20:41:16 +0600

    Login page's Logic added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,17 @@
-console.log('file has been connected')
+document.getElementById('login-btn').addEventListener('click', function(){
+    const mobileInput = document.getElementById('mobile-input');
+    const mobileNumber = mobileInput.value;
+    console.log(mobileNumber)
+    const pinInput = document.getElementById('pin-input');
+    const Pin = pinInput.value;
+    console.log(Pin)
+    if(mobileNumber == '01402723682' && Pin == '1234' ){
+        alert('Login Successful')
+        mobileInput.value = '';
+        pinInput.value = '';
+        window.location.assign("/Home.html");
+    } else{
+        alert('Wrong Credential, Try Again')
+        return;
+    }
+})
