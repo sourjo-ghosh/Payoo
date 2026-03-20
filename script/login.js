2cb06353d1e9228de592f3201fb85591355486d1
sourjo-ghosh <joyghosh211@outlook.com>
2026-03-20 06:48:21 +0600

    login bug fixs and demo credential added

--- script/login.js
+++ script/login.js
@@ -2,13 +2,13 @@ document.getElementById('login-btn').addEventListener('click', function () {
     const mobileInput = document.getElementById('mobile-input');
     const mobileNumber = mobileInput.value;
     console.log(mobileNumber)
-    const cash-out-pinInput = document.getElementById('cash-out-pin-input');
-    const Cash-out-pin = cash-out-pinInput.value;
+    const pinInput = document.getElementById('pin-input');
+    const pin = pinInput.value;
     const myModal = document.getElementById("my_modal_3")
-    if (mobileNumber == '01407273682' && Cash-out-pin == '1234') {
+    if (mobileNumber == '01407273682' && pin == '1234') {
         myModal.showModal()
         mobileInput.value = '';
-        cash-out-pinInput.value = '';
+        pinInput.value = '';
     } else {
         alert('Wrong Credential, Try Again')
         return;
